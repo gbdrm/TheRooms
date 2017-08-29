8368fb81c3afb4952a68122712e6fc1b6cc40369
Viktor Kotsiuban <gbdrm1@gmail.com>
2017-08-29 13:00:45 +0300

    Add rooms and doors model

---
+++ Models/Door.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheRooms.Models
+{
+    public class Door
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("RoomFrom")]
+        public int RoomFromId { get; set; }
+        public Room RoomFrom { get; set; }
+
+        [ForeignKey("RoomTo")]
+        public int RoomToId { get; set; }
+        public Room RoomTo { get; set; }
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace TheRooms.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Task { get; set; }
+        public string Answer { get; set; }
+    }
+}
